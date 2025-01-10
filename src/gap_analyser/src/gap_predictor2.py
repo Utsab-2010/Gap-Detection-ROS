defa7b29154d84735317f6aad815e00c0842af3a
Utsab-2010 <utsabkaran2010@gmail.com>
2025-01-10 23:18:10 +0530

    added a timer to iterate a intervals of time

--- src/gap_analyser/src/gap_predictor2.py
+++ src/gap_analyser/src/gap_predictor2.py
@@ -6,14 +6,22 @@ from std_msgs.msg import Float32MultiArray
 from sensor_msgs.msg import LaserScan
 import math
 
+def get_gap(pos_1,pos_2):
+    return ((pos_1[0] - pos_2[0])**2+(pos_1[1]-pos_2[1])**2)**0.5
+
+
+
 class Gap_Computation_Node:
     def __init__(self):
         rospy.init_node('gap_predictor_node')
 
-        self.state_sub = rospy.Subscriber('/gazebo/model_states',ModelStates,self.model_state_callback)
-        self.lidar_sub = rospy.Subscriber('/scan',LaserScan,self.lidar_callback)
-
+        self.state_sub = rospy.Subscriber('/gazebo/model_states',ModelStates,self.state_sub_callback)
+        self.latest_state_msg=None
+        self.lidar_sub = rospy.Subscriber('/scan',LaserScan,self.lidar_sub_callback)
+        self.latest_lidar_msg=None
         self.pub = rospy.Publisher('predicted_gaps',Float32MultiArray)
+
+        self.timer = rospy.Timer(rospy.Duration(0.1), self.process_message) #0.1 s per iteration
 
         self.real_gap=0
         self.cv_model_gap=0
@@ -21,24 +29,30 @@ class Gap_Computation_Node:
 
         self.cylinder_radius=0.1
         self.pos_list = []
+    
+    def state_sub_callback(self,msg):
+        self.latest_state_msg = msg
+    
+    def lidar_sub_callback(self,msg):
+        self.latest_lidar_msg = msg
 
-    def model_state_callback(self,msg):
+    def real_gaps(self,msg):
     #getting actual position of the obstacles
-        pos = [0,0,0,0,0,0]
+        pos = [(),(),()]
         for i, name in enumerate(msg.name):
             if name == "cylinder_1":  # Replace with your model's name
                 # Extract position (x, y, z) of the model
-                pos[0] = msg.pose[i].position.x
-                pos[1] = msg.pose[i].position.y
+                pos[0] = (msg.pose[i].position.x,msg.pose[i].position.y)
             if name == "cylinder_2":
-                pos[2] = msg.pose[i].position.x
-                pos[3] = msg.pose[i].position.y
+                pos[1] = (msg.pose[i].position.x,msg.pose[i].position.y)
             if name == "cylinder_3":
-                pos[4] = msg.pose[i].position.x
-                pos[5] = msg.pose[i].position.y   
+                pos[2] = (msg.pose[i].position.x,msg.pose[i].position.y)   
 
-        self.real_gap = ((pos[0]-pos[2])**2 + (pos[1]-pos[3])**2)**0.5
-        print(f"1st Pos: {pos[0],pos[1]}",f"2st Pos: {pos[2],pos[3]}")
+        self.real_gap = (get_gap(pos[0],pos[1]),
+                         get_gap(pos[1],pos[2]),
+                         get_gap(pos[2],pos[0]))
+        
+        # print(f"1st Pos: {pos[0],pos[1]}",f"2st Pos: {pos[2],pos[3]}")
 
     def lidar_callback(self,msg):
         point_cloud= []
@@ -51,9 +65,15 @@ class Gap_Computation_Node:
         
         sim_time = rospy.Time.now()
 
-        self.pos_list.append((pos_1,pos_2,sim_time))
+        self.pos_list.append((pos_1,pos_2,pos_3,sim_time))
         if len(self.pos_list >5):
             self.pos_list.pop(0)
+
+    def process_message(self):
+        
+        self.real_gap(self.latest_state_msg)
+
+    
         
     def run(self):
         rospy.spin()
---
+++ src/gap_analyser/src/prediction_models.py
@@ -0,0 +1,5 @@
+import math 
+
+
+def velocity_model(pos_list):
+    pos_1,pos_2,pos_3,
